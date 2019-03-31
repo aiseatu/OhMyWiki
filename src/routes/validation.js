45440f0b7fbf8386c71899930387ae14df3c8a9d
Xiaokang Tang <xiaokangt@hotmail.com>
2019-03-31 17:50:02 -0400

    do not log error

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -7,7 +7,7 @@ module.exports = {
     }
     const errors = req.validationErrors();
     if(errors) {
-      console.log(errors);
+      //console.log(errors);
       req.flash("error", errors);
       return res.redirect(req.headers.referer);
     } else {
